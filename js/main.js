c80289e56f76befed331affaffed233c2ce2dfec
admin <jsbparra@gmail.com>
2019-07-06 13:32:10 +0200

    WIP

--- js/main.js
+++ js/main.js
@@ -8,8 +8,6 @@ document.onload = (function() {
   game = new Game(ctx);
   game.start();
 })();
-/* TENGO UN PROBLEMA PARA HACER COMMIT, HE PROBADO CON OTRO  PROYECTO
-Y ME HACE EL COMMIT PERO EN EL DE GAME NO .. */
 
 // hacer que el cuadrado salte
 // poner limites a la izquierda del screen
