29f7a2d12f8e96bf6aff3603128defca34d6cfc7
admin <jsbparra@gmail.com>
2019-07-11 06:17:29 +0200

    it.7 collision

--- js/game.js
+++ js/game.js
@@ -35,6 +35,53 @@ class Game {
     };
   }
 
+  colision() {
+    // hacer colision general y por elemento se ejecute por cada clase.
+    if (
+      // lucky vs floor
+      this.lucky.x < this.floor.x + this.floor.width &&
+      this.lucky.x + this.lucky.width > this.floor.x &&
+      this.lucky.y < this.floor.y &&
+      this.lucky.height + this.lucky.y > this.floor.y
+    ) {
+      this.lucky.y = this.floor.y - this.lucky.height;
+    }
+
+    if (
+      //lucky vs danger
+      this.lucky.x < this.danger.x + this.danger.width &&
+      this.lucky.x + this.lucky.width > this.danger.x &&
+      this.lucky.y < this.danger.y + this.danger.height &&
+      this.lucky.height + this.lucky.y > this.danger.y
+    ) {
+      this.gameOver(); // cuando toque que salga anuncio gameover y boton reset!
+    }
+    if (
+      // lucky vs bonus
+      this.lucky.x < this.bonus.x + this.bonus.width &&
+      this.lucky.x + this.lucky.width > this.bonus.x &&
+      this.lucky.y < this.bonus.y + this.bonus.height &&
+      this.lucky.height + this.lucky.y > this.bonus.y
+    ) {
+      console.log("Bonus");
+    }
+    if (
+      //lucky vs goal
+      this.lucky.x < this.goal.x + this.goal.width &&
+      this.lucky.x + this.lucky.width > this.goal.x &&
+      this.lucky.y < this.goal.y + this.goal.height &&
+      this.lucky.height + this.lucky.y > this.goal.y
+    ) {
+      console.log("winner");
+    }
+    if (
+      //lucky vs canvas
+      this.lucky.x < 0 ||
+      this.lucky.y < 0
+    ) {
+      this.gameOver();
+    }
+  }
   update() {
     this.ctx.clearRect(0, 0, this.canvasWidth, this.canvasHeight);
     this.drawBoard();
@@ -47,6 +94,7 @@ class Game {
     this.danger.draw();
     this.lucky.update();
     this.lucky.draw();
+    this.colision();
     window.requestAnimationFrame(this.update.bind(this));
   }
 
@@ -54,4 +102,15 @@ class Game {
     this.assignControls();
     this.update();
   }
+
+  gameOver() {
+    console.log("game over!");
+  }
+
+  stop() {
+    // 1. funcion stop.
+    console.log("space-pausa el juego!");
+  }
+
+  score() {}
 }
